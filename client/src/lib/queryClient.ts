7762b2490da305521c714ed41f89f290d14d1b77
Astha <47259798+ASTHA22@users.noreply.github.com>
2024-12-18 02:10:17 -0800

    Add files via upload

---
+++ client/src/lib/queryClient.ts
@@ -0,0 +1,33 @@
+import { QueryClient } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      queryFn: async ({ queryKey }) => {
+        const baseUrl = process.env.NODE_ENV === 'production' 
+          ? process.env.VERCEL_URL || ''
+          : '';
+        const res = await fetch(`${baseUrl}${queryKey[0]}`, {
+          credentials: "include",
+        });
+
+        if (!res.ok) {
+          if (res.status >= 500) {
+            throw new Error(`${res.status}: ${res.statusText}`);
+          }
+
+          throw new Error(`${res.status}: ${await res.text()}`);
+        }
+
+        return res.json();
+      },
+      refetchInterval: false,
+      refetchOnWindowFocus: false,
+      staleTime: Infinity,
+      retry: false,
+    },
+    mutations: {
+      retry: false,
+    }
+  },
+});
